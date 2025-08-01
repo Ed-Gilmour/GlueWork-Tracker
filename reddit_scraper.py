e59d5130b43ea75c1bd1b4ed53cd30606a785e74
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-08-01 16:06:23 -0700

    Top Posters

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import requests
 
 def fetch_reddit_posts(subreddit, limit=10):
@@ -6,14 +7,40 @@ def fetch_reddit_posts(subreddit, limit=10):
 
     response = requests.get(url, headers=headers)
     data = response.json()
+
+    if(response.status_code != 200):
+        print("error code: ", response.status_code)
+        return
+
+    post_counter = Counter()
+    reply_counter = Counter()
+
     posts = data["data"]["children"]
 
     for i, post in enumerate(posts):
-        title = post["data"]["title"]
-        author = post["data"]["author"]
-        score = post["data"]["score"]
-        permalink = post["data"]["permalink"]
-        print(f"{i+1}. {title} | name: {author} | score: {score}")
-        print(f"https://reddit.com{permalink}\n")
+        post_counter[post["data"]["author"]] += 1
+        top_reply_user = get_top_reply_user(subreddit=subreddit, id=post["data"]["id"])
+        if top_reply_user != None:
+            reply_counter[top_reply_user] += 1
+    for i, (user, posts) in enumerate(post_counter.most_common(10)):
+        print(f"{i+1}. {user} | posts: {posts} | replies: {reply_counter.get(user)}")
 
-fetch_reddit_posts("learnprogramming", limit=5)
+def get_top_reply_user(subreddit, id):
+    headers = {'User-Agent': 'MyRedditScraper/1.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/comments/{id}.json?sort=top&limit=1"
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print("Error:", response.status_code)
+        return
+
+    data = response.json()
+    comments = data[1]["data"]["children"]
+
+    if len(comments) == 0:
+        return None
+
+    comment = data[1]["data"]["children"][0]
+    return comment["data"]["author"]
+
+fetch_reddit_posts("FlutterDev", limit=50)
