9c27d0943842a25fe2ebb50ecab41280713ef76e
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-08-08 13:37:45 -0700

    Required fields

--- glue_work_bot/config_handler.py
+++ glue_work_bot/config_handler.py
@@ -6,12 +6,18 @@ class ConfigHandler:
         self.config_path = config_path
         self.config = None
         self.load_exception = None
+        self.required_fields = ['urls']
 
     def load_config(self): # Returns True if successful, otherwise returns False
         try:
             with open(self.config_path, 'r') as f:
                 self.config = yaml.safe_load(f)
-                return True
+                missing = [key for key in self.required_fields if key not in self.config]
+                if missing:
+                    self.load_exception = "Missing required fields in config.\nRequired fields:"
+                    for field in self.required_fields:
+                        self.load_exception += "\n" + field
+                return missing
         except Exception as e:
             self.load_exception = e
             return False
