7e8f4847c6e07ac0d30b35ab29c720d786a645b6
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-08-13 16:01:55 -0700

    bot cleanup

--- glue_work_bot/main.py
+++ glue_work_bot/main.py
@@ -1,38 +1,8 @@
-# from github_scraper import GitHubScraper
-# from classifier_agents import ClassifierAgent
 from config_handler import ConfigHandler
 from output_handler import OutputHandler
 import argparse
 
-# def main():
-#     github_scraper = GitHubScraper()
-#     classifier_agent = ClassifierAgent()
-#     issues = github_scraper.get_issues(per_page=3)
-#     for issue in issues:
-#         print("\n-------------------ISSUE CLASSIFICATION-------------------\n")
-#         issue_str = github_scraper.get_issue_str(issue)
-#         if issue_str == "":
-#             print("Bot, continuing...")
-#             continue
-#         response = classifier_agent.classify_data(issue_str)
-#         print("\n" + issue_str + "\n")
-#         print("\n-------------------CLASSIFICATION-------------------\n")
-#         print(response)
-#         print("\n-------------------ISSUE CLASSIFICATION END-------------------\n")
-#     pull_requests = github_scraper.get_pull_requests(per_page=3)
-#     for pull_request in pull_requests:
-#         print("\n-------------------PULL REQUEST CLASSIFICATION-------------------\n")
-#         pull_request_str = github_scraper.get_pull_request_str(pull_request)
-#         if pull_request_str == "":
-#             print("Bot, continuing...")
-#             continue
-#         response = classifier_agent.classify_data(pull_request_str)
-#         print("\n" + pull_request_str + "\n")
-#         print("\n-------------------CLASSIFICATION-------------------")
-#         print(response)
-#         print("\n-------------------PULL REQUEST CLASSIFICATION END-------------------\n")
-
-def test():
+def run_bot():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config-file', required=True)
     parser.add_argument('--output-dir', required=True)
@@ -44,4 +14,4 @@ def test():
     output_handler.save_output()
 
 if __name__ == "__main__":
-    test()
+    run_bot()
