bef713dcd6d89d4872dd56313d061cd85da2dc11
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-08-19 10:30:35 -0700

    Commit and PR classifications

--- glue_work_bot/classifier_agents.py
+++ glue_work_bot/classifier_agents.py
@@ -11,7 +11,7 @@ class CodeAgent:
     def get_issue_prompt(self, issue):
         return f"""
 You are given a GitHub issue.
-You specialize in classifying whether this issue is maintenance or quality assurance.
+You specialize in classifying whether this issue is maintenance, quality assurance, if it's anything else respond with unknown.
 Respond with only the classification of the issue.
 
 Issue Title:
@@ -19,6 +19,29 @@ Issue Title:
 
 Issue Body:
 {issue["body"]}
+"""
+
+def get_pull_request_prompt(self, pull_request):
+    return f"""
+You are given a GitHub pull request.
+You specialize in classifying whether this pull request is maintenance, quality assurance, if it's anything else respond with unknown.
+Respond with only the classification of the pull request.
+
+Pull Request Title:
+{pull_request["title"]}
+
+Pull Request Body:
+{pull_request["body"]}
+"""
+
+def get_commit_prompt(self, commit):
+    return f"""
+You are given a GitHub commit.
+You specialize in classifying whether this commit is maintenance, quality assurance, if it's anything else respond with unknown.
+Respond with only the classification of the commit.
+
+Commit Message:
+{commit["message"]}
 """
 
 def strip_think_tags(response: str) -> str:
--- glue_work_bot/work_distributor.py
+++ glue_work_bot/work_distributor.py
@@ -12,3 +12,13 @@ class WorkDistributor:
             self.output.add_contributor(issue["author"])
             if i < 3:
                 self.output.add_classification(code_agent.classify_data(code_agent.get_issue_prompt(issue)))
+        for i in range(len(self.data["pull_requests"])):
+            pull_request = self.data["pull_requests"][i]
+            self.output.add_contributor(pull_request["author"])
+            if i < 3:
+                self.output.add_classification(code_agent.classify_data(code_agent.get_pull_request_prompt(pull_request)))
+        for i in range(len(self.data["commits"])):
+            commit = self.data["commits"][i]
+            self.output.add_contributor(commit["author"])
+            if i < 3:
+                self.output.add_classification(code_agent.classify_data(code_agent.get_commit_prompt(commit)))
