f4245d0b621b30f9eb89217a52ee0dd5e6e95f4a
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-08-19 11:13:59 -0700

    Classifier fix

--- glue_work_bot/classifier_agents.py
+++ glue_work_bot/classifier_agents.py
@@ -6,7 +6,7 @@ class CodeAgent:
         self.model = OllamaLLM(model="deepseek-r1:7b", base_url="http://localhost:11434")
 
     def classify_data(self, prompt):
-        return strip_think_tags(self.model.invoke(prompt))
+        return self.strip_think_tags(self.model.invoke(prompt))
 
     def get_issue_prompt(self, issue):
         return f"""
@@ -21,28 +21,28 @@ Issue Body:
 {issue["body"]}
 """
 
-def get_pull_request_prompt(self, pull_request):
-    return f"""
-You are given a GitHub pull request.
-You specialize in classifying whether this pull request is maintenance, quality assurance, if it's anything else respond with unknown.
-Respond with only the classification of the pull request.
+    def get_pull_request_prompt(self, pull_request):
+        return f"""
+    You are given a GitHub pull request.
+    You specialize in classifying whether this pull request is maintenance, quality assurance, if it's anything else respond with unknown.
+    Respond with only the classification of the pull request.
 
-Pull Request Title:
-{pull_request["title"]}
+    Pull Request Title:
+    {pull_request["title"]}
 
-Pull Request Body:
-{pull_request["body"]}
-"""
+    Pull Request Body:
+    {pull_request["body"]}
+    """
 
-def get_commit_prompt(self, commit):
-    return f"""
-You are given a GitHub commit.
-You specialize in classifying whether this commit is maintenance, quality assurance, if it's anything else respond with unknown.
-Respond with only the classification of the commit.
+    def get_commit_prompt(self, commit):
+        return f"""
+    You are given a GitHub commit.
+    You specialize in classifying whether this commit is maintenance, quality assurance, if it's anything else respond with unknown.
+    Respond with only the classification of the commit.
 
-Commit Message:
-{commit["message"]}
-"""
+    Commit Message:
+    {commit["message"]}
+    """
 
-def strip_think_tags(response: str) -> str:
-    return re.sub(r"<think>.*?</think>", "", response, flags=re.DOTALL)
+    def strip_think_tags(self, text):
+        return re.sub(r"<think>.*?</think>", "", text, flags=re.DOTALL)
