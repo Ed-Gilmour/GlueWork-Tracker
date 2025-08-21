c5ccbcfee585057c4e15e69789fc6442534081ac
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-08-21 16:03:28 -0700

    Removed logs

--- glue_work_bot/work_distributor.py
+++ glue_work_bot/work_distributor.py
@@ -13,21 +13,18 @@ class WorkDistributor:
             classification = GlueWorkType.UNKNOWN
             if i < 3:
                 classification = code_agent.classify_data(code_agent.get_issue_prompt(issue))
-            print(f"Issue {i}: {classification.get_label()}", flush=True)
             self.aggregator.add_work(issue["author"], classification)
         for i in range(len(self.data["pull_requests"])):
             pull_request = self.data["pull_requests"][i]
             classification = GlueWorkType.UNKNOWN
             if i < 3:
                 classification = code_agent.classify_data(code_agent.get_pull_request_prompt(pull_request))
-            print(f"Pull Request {i}: {classification.get_label()}", flush=True)
             self.aggregator.add_work(pull_request["author"], classification)
         for i in range(len(self.data["commits"])):
             commit = self.data["commits"][i]
             classification = GlueWorkType.UNKNOWN
             if i < 3:
                 classification = code_agent.classify_data(code_agent.get_commit_prompt(commit))
-            print(f"Commit {i}: {classification.get_label()}", flush=True)
             self.aggregator.add_work(commit["author"], classification)
         for i in range(len(self.data["reviews"])):
             review = self.data["reviews"][i]
