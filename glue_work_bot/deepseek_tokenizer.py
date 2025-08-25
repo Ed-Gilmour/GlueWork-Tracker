f2c12fd310578b089a8fa8a15d0760292c74eab7
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-08-25 14:28:28 -0700

    Tokenizer fix

--- glue_work_bot/deepseek_tokenizer.py
+++ glue_work_bot/deepseek_tokenizer.py
@@ -4,7 +4,7 @@ class DeepseekTokenizer:
 	TOKEN_LIMIT = 120000
 
 	def __init__(self):
-		chat_tokenizer_dir = "C:/Users/edwar/OneDrive/Desktop/Programming Projects/GlueWork-Tracker/glue_work_bot"
+		chat_tokenizer_dir = "./glue_work_bot"
 		self.tokenizer = transformers.AutoTokenizer.from_pretrained(
 				chat_tokenizer_dir, trust_remote_code=True
 		)
