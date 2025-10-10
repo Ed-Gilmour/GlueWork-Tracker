ef6c65cd8ea64f65ea6e17e17c25181e29c90183
Ed-Gilmour <edwardmgilmour@gmail.com>
2025-10-09 17:36:41 -0700

    Community managment test

--- glue_work_bot/work_distributor.py
+++ glue_work_bot/work_distributor.py
@@ -7,35 +7,35 @@ class WorkDistributor:
         self.data = data
 
     def distribute_work(self):
-        github_data = self.data["github"]
+        # github_data = self.data["github"]
         stackexchange_data = self.data["stackexchange"]
-        code_agent = CodeAgent(self.aggregator)
-        for i in range(len(github_data["issues"])):
-            issue = github_data["issues"][i]
-            classification = GlueWorkType.UNKNOWN
-            if i < 3:
-                classification = code_agent.classify_data(code_agent.get_issue_prompt(issue))
-            self.aggregator.add_work(issue["author"], classification)
-        for i in range(len(github_data["pull_requests"])):
-            pull_request = github_data["pull_requests"][i]
-            classification = GlueWorkType.UNKNOWN
-            if i < 3:
-                classification = code_agent.classify_data(code_agent.get_pull_request_prompt(pull_request))
-            self.aggregator.add_work(pull_request["author"], classification)
-        for i in range(len(github_data["commits"])):
-            commit = github_data["commits"][i]
-            classification = GlueWorkType.UNKNOWN
-            if i < 3:
-                classification = code_agent.classify_data(code_agent.get_commit_prompt(commit))
-            self.aggregator.add_work(commit["author"], classification)
+        # code_agent = CodeAgent(self.aggregator)
+        # for i in range(len(github_data["issues"])):
+        #     issue = github_data["issues"][i]
+        #     classification = GlueWorkType.UNKNOWN
+        #     if i < 3:
+        #         classification = code_agent.classify_data(code_agent.get_issue_prompt(issue))
+        #     self.aggregator.add_work(issue["author"], classification)
+        # for i in range(len(github_data["pull_requests"])):
+        #     pull_request = github_data["pull_requests"][i]
+        #     classification = GlueWorkType.UNKNOWN
+        #     if i < 3:
+        #         classification = code_agent.classify_data(code_agent.get_pull_request_prompt(pull_request))
+        #     self.aggregator.add_work(pull_request["author"], classification)
+        # for i in range(len(github_data["commits"])):
+        #     commit = github_data["commits"][i]
+        #     classification = GlueWorkType.UNKNOWN
+        #     if i < 3:
+        #         classification = code_agent.classify_data(code_agent.get_commit_prompt(commit))
+        #     self.aggregator.add_work(commit["author"], classification)
 
-        mentoring_agent = MentoringAgent(self.aggregator)
-        for i in range(len(github_data["comments"])):
-            comment = github_data["comments"][i]
-            classification = GlueWorkType.UNKNOWN
-            if i < 3:
-                classification = mentoring_agent.classify_data(mentoring_agent.get_comment_prompt(comment))
-            self.aggregator.add_work(comment["author"], classification)
+        # mentoring_agent = MentoringAgent(self.aggregator)
+        # for i in range(len(github_data["comments"])):
+        #     comment = github_data["comments"][i]
+        #     classification = GlueWorkType.UNKNOWN
+        #     if i < 3:
+        #         classification = mentoring_agent.classify_data(mentoring_agent.get_comment_prompt(comment))
+        #     self.aggregator.add_work(comment["author"], classification)
 
         community_agent = CommunityAgent(self.aggregator)
         for i in range(len(stackexchange_data["posts"])):
@@ -45,11 +45,11 @@ class WorkDistributor:
                 classification = community_agent.classify_data(community_agent.get_community_managment_prompt(post))
             self.aggregator.add_work(post["author"], classification)
 
-        for i in range(len(github_data["reviews"])):
-            review = github_data["reviews"][i]
-            self.aggregator.add_work(review["author"], GlueWorkType.CODE_REVIEW)
-        for i in range(len(github_data["documentation_changes"])):
-            documentation_change = github_data["documentation_changes"][i]
-            self.aggregator.add_work(documentation_change["author"], GlueWorkType.DOCUMENTATION)
+        # for i in range(len(github_data["reviews"])):
+        #     review = github_data["reviews"][i]
+        #     self.aggregator.add_work(review["author"], GlueWorkType.CODE_REVIEW)
+        # for i in range(len(github_data["documentation_changes"])):
+        #     documentation_change = github_data["documentation_changes"][i]
+        #     self.aggregator.add_work(documentation_change["author"], GlueWorkType.DOCUMENTATION)
 
         self.aggregator.output_work()
